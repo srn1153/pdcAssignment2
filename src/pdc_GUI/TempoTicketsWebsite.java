78cd4cdfdd8f3af389cef10a8f3d82856d58f0a9
srn1153 <srn1153@autuni.ac.nz>
2024-05-13 19:56:02 +1200

    Initial set up!

---
+++ src/pdc_GUI/TempoTicketsWebsite.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author nikis
+ */
+public class TempoTicketsWebsite extends JFrame {
+    private JPanel currPanel; 
+    
+}
