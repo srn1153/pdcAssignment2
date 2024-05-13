4bbf98d3b27308fcead862c2232c7c93b40279ae
srn1153 <srn1153@autuni.ac.nz>
2024-05-13 20:10:03 +1200

    Some frame and class setups

---
+++ src/pdc_GUI/HomepagePanel.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_GUI;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author nikis
+ */
+public class HomepagePanel extends JPanel {
+    private TempoTicketsWebsite ttw; 
+    
+    public HomepagePanel(TempoTicketsWebsite ttw){
+        this.ttw = ttw; 
+        HomePage(); 
+    }
+    
+    public void HomePage() {
+        
+    }
+    
+}
--- src/pdc_GUI/TempoTicketsWebsite.java
+++ src/pdc_GUI/TempoTicketsWebsite.java
@@ -14,4 +14,17 @@ import javax.swing.JPanel;
 public class TempoTicketsWebsite extends JFrame {
     private JPanel currPanel; 
     
+    public TempoTicketsWebsite() {
+        setTitle("Tempo Tickets Website!"); 
+        setSize(800, 800); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setLocationRelativeTo(null); 
+        
+        //creating homepage window 
+        HomepagePanel hpp = new HomepagePanel(this); 
+        currPanel = hpp; 
+        add(hpp); 
+        
+        setVisible(true); 
+    }
 }
