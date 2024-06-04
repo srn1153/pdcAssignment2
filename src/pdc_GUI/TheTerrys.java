93fc035e8e3d7e7772c3acd62d4e0639e07d75d2
srn1153 <srn1153@autuni.ac.nz>
2024-06-04 14:46:11 +1200

    Adding class back

---
+++ src/pdc_GUI/TheTerrys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_GUI;
+
+/**
+ *
+ * @author nikis
+ */
+public class TheTerrys extends ArtistPanel {
+    
+    public TheTerrys(TempoTicketsWebsite ttw){
+        super(ttw, new Artist("The Terrys","Indie Rock", "Tuning Fork", "27th August 2024", "7pm-10pm", 44.99)); 
+    }
+}
