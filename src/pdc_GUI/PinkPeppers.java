2a8bc5b39816305ec9275cef17611f86f3cecead
srn1153 <srn1153@autuni.ac.nz>
2024-06-04 14:46:41 +1200

    Adding class back

---
+++ src/pdc_GUI/PinkPeppers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_GUI;
+
+/**
+ *
+ * @author nikis
+ */
+public class PinkPeppers extends ArtistPanel {
+    
+    public PinkPeppers(TempoTicketsWebsite ttw){
+        super(ttw, new Artist("Pink Peppers", "Kids Musical", "Town Hall", "3rd September 2024", "5pm-7pm", 12.0)); 
+    }
+}
